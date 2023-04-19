163d0df349a98fd59513ef45eb05986b1758a2fd
iManapov <44193789+iManapov@users.noreply.github.com>
2023-04-19 09:45:22 +0300

    Fixed rating and chars_count methods using map

--- maps/maps.py
+++ maps/maps.py
@@ -13,7 +13,18 @@ class MapExercise:
         Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
-        pass
+
+        filtered_movies = list(
+            filter(lambda x: x is not None, map(MapExercise.filter_movies, list_of_movies))
+        )
+
+        return sum(filtered_movies) / len(filtered_movies)
+
+    @staticmethod
+    def filter_movies(movie: dict) -> float:
+        count_country = len(movie["country"].split(","))
+        rating = movie["rating_kinopoisk"]
+        return float(rating) if rating and float(rating) > 0 and count_country > 1 else None
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
@@ -28,4 +39,11 @@ class MapExercise:
         :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
         или равным заданному значению
         """
-        pass
+
+        return sum(map(lambda x: MapExercise.count_chars_in_movie(x, "и", rating), list_of_movies))
+
+    @staticmethod
+    def count_chars_in_movie(movie: dict, char: str, rating: Union[float, int]) -> int:
+        if movie["rating_kinopoisk"] and float(movie["rating_kinopoisk"]) >= rating:
+            return movie["name"].count(char)
+        return 0
